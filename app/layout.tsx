712fe082c7e488ff3dd8f5e02cac924bd0859966
Ritik <ritik@logiqlink.com>
2026-05-06 18:25:27 +0530

    Added theme vars, installed next-themes, modified layout.tsx,

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,36 @@
 import './globals.css';
 import Providers from '../store/provider';
+import { ThemeProvider } from '@/components/ThemeProvider';
+import { Poppins } from "next/font/google";
+import type { Metadata } from "next";
+
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-poppins",
+});
+
+export const metadata: Metadata = {
+  title: "PayU - Payment Gateway",
+  description: "PayU is a payment gateway that allows you to pay for goods and services online.",
+  icons: {
+    icon: "/logo.png",
+  },
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>
-        <Providers>{children}</Providers>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${poppins.className} custom-scrollbar`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Providers>{children}</Providers>
+        </ThemeProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import PaymentForm from '../components/payment/PaymentForm';
 
 export default function Page() {
   return (
-    <main>
+    <main className='bg-bg-primary text-text-primary'>
       <h1>Payment Gateway</h1>
       <PaymentForm />
     </main>
